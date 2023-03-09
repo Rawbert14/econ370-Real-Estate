d486f2e656cabe62b542b623d6646c6fafb0bf39
tdkelley2001 <94010992+tdkelley2001@users.noreply.github.com>
2023-03-09 14:29:20 -0500

    Update and rename map.py to clean_map.py

--- clean_map.py
+++ clean_map.py
@@ -3,7 +3,14 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 # read in the csv file as a pandas dataframe
-data = pd.read_csv('zillow_housing.csv')
+data = pd.read_csv('Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv', 
+                   skiprows=[1])
+
+# drop cities with missing data
+data = data.dropna()
+
+# split the city names and state abbreviations
+data[['RegionName', 'State']] = data['RegionName'].str.split(', ', expand=True)
 
 # extract the list of city names from the desired column
 city_names = data['RegionName'].tolist()
@@ -14,6 +21,12 @@ cities = gpd.read_file("USA_Major_Cities/USA_Major_Cities.shp")
 
 # filter the cities shapefile to only include the cities in the list of city names
 cities_to_plot = cities[cities['NAME'].isin(city_names)]
+
+# filter out Alaska and Hawaii if necessary
+if "AK" not in list(cities_to_plot["ST"]):
+    states = states.drop(1)
+if "HI" not in list(cities_to_plot["ST"]):
+    states = states.drop(11)
 
 # plot the cities and the state boundaries
 ax = states.plot(color='white', edgecolor='black', linewidth=0.5)
